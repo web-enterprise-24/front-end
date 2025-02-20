c2a7d0d7c90940ac4acab01ce3593160ac7b1540
Nguyen Quoc Vuong <121931993+NQuVuong@users.noreply.github.com>
2025-02-20 16:06:16 +0700

    update side bar

--- src/pages/Management/Management.tsx
+++ src/pages/Management/Management.tsx
@@ -9,7 +9,7 @@ const Management = () => {
    <NavbarOnlyAvatar />
    <div className="flex flex-row justify-between w-full bg-base-300">
     <SidebarFixed items={SidebarManagementItems} />
-    <div className="w-full md:w-5/6 h-[calc(100vh-64px)] ml-1 mt-1 shadow-md shadow-base-100 bg-base-100 rounded-2xl">
+    <div className="w-full h-[calc(100vh-64px)] ml-1 mt-1 shadow-md shadow-base-100 bg-base-100 rounded-2xl">
      <Outlet />
     </div>
    </div>
