dcc4ddcbf429df3724a871520e4300bbe6022542
Nguyen Quoc Phong <phongnqgcc210026@fpt.edu.vn>
2025-03-20 10:37:57 +0700

    beautiful morning

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-// import { StrictMode } from "react";
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
@@ -6,9 +6,9 @@ import './index.css';
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
-	//  <StrictMode>
-	//  </StrictMode>
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>
+	<StrictMode>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</StrictMode>
 );
